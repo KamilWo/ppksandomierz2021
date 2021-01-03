18d384bbf4b05f63e6271ae665a517efd80dc50a
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2021-01-03 10:30:40 +0000

    Initial commit

---
+++ footer.php
@@ -0,0 +1,10 @@
+<!-- stopka -->
+<footer>
+    <p class="text-left tiny">Aktualizacja: 27.&nbsp;grudnia 2020&nbsp;r.</p>
+    <p>Parafia Podwyższenia Krzyża Świętego &copy;
+       2012-<script>document.write(new Date().getFullYear());</script>
+       Wszelkie prawa zastrzeżone.<br />
+       Przygotował K.&nbsp;Woźniak
+    </p>
+</footer>
+<!-- koniec stopki -->
---
+++ right_pane.php
@@ -0,0 +1,70 @@
+<!-- prawy panel -->
+<div class="col-md-4 rightPanel">
+    <h3>
+        Św.&nbsp;Mikołaj w&nbsp;Podwyższeniu
+    </h3>
+    <p>
+        <img src="static/download/Swiety_Mikolaj_z_Miry.webp" alt="Święty Mikołaj z Miry"/>
+    </p>
+    <p>
+        Zapraszamy wszystkie dzieci z&nbsp;naszej parafii, które nie
+        ukończyły 11&nbsp;lat życia, do&nbsp;uczczenia osoby
+        św.&nbsp;Mikołaja biskupa. <strong>Celem tej inicjatywy jest ukazanie prawdy
+        o&nbsp;wielkim świętym Kościoła, który uczy nas czym jest miłość,
+        dobro i&nbsp;miłosierdzie.</strong>
+    </p>
+    <p>
+        <strong>Forma prezentacji</strong>: film, max 3&nbsp;min. (piosenka, wiersz,
+        krótkie opowiadanie).
+    </p>
+    <p>
+        <strong>Wiek uczestników</strong>: dzieci do&nbsp;11&nbsp;roku życia włącznie.
+    </p>
+    <p>
+        <strong>Termin nadsyłania prac</strong>: 30&nbsp;listopada 2020&nbsp;r.
+    </p>
+    <p>
+        <strong>Adres przesyłania prac</strong>: <a href="mailto:ppksandomierz@onet.pl">
+        ppksandomierz@onet.pl</a> – w&nbsp;tytule widomości prosimy wpisać:
+    </p>
+    <p>
+        <strong># Mikołajki w&nbsp;Podwyższeniu</strong>.
+    </p>
+    <p>
+        W&nbsp;treści wiadomości prosimy podać imię, nazwisko i&nbsp;adres
+        celem weryfikacji – wydarzenie dotyczy tylko naszych parafian.
+    </p>
+    <p>
+        Dla wszystkich uczestników mikołajkowy prezent.
+    </p>
+    <h3>
+        Dekret<br />
+        Zasady obowiązujące w związku z nową falą pandemii koronawirusa
+    </h3>
+    <p>
+        17&nbsp;października 2020&nbsp;r. Biskup Sandomierski Krzysztof
+        Nitkiewicz wydał <strong>ważny dekret</strong>
+        w&nbsp;związku z&nbsp;nową falą pandemii koronawirusa.<br />
+        Jest on&nbsp;dostępny pod adresem internetowym Diecezji Sandomierskiej:
+        <a href="https://diecezjasandomierska.pl/zasady-obowiazujace-w-zwiazku-z-nowa-fala-pandemii-koronawirusa/"   title="Dekret w związku z nową falą pandemii">link do dekretu</a>
+    </p>
+    <h3>
+        Galeria I&nbsp;Komunii Świętej 2020
+    </h3>
+    <p>
+        W&nbsp;Galerii znajdują się zdjęcia z&nbsp;uroczystości
+        I&nbsp;Komunii Świętej.
+    </p>
+    <p>
+        <a href="https://diecezjasandomierska.pl/dekret-o-obowiazku-uczestnictwa-we-mszy-sw/">
+        Link do dekretu na stronie Diecezji Sandomierskiej</a><br />
+        <a href="static/download/Dekret-o-obowiazku-uczestnictwa-we-Mszy-sw_2020-05-29.pdf">
+        Plik do pobrania</a>
+    </p>
+    <h3>25-lecie Parafii</h3>
+    <p class="text-left">W dniach <strong>14-17 czerwca 2012&nbsp;r.</strong> odbyły się uroczystości liturgiczne
+        z&nbsp;okazji obchodów Jubileuszu 25-lecia Parafii p.w. Podwyższenia Krzyża Świętego
+        w&nbsp;Sandomierzu.
+    </p>
+</div>
+<!-- koniec prawego panelu -->
