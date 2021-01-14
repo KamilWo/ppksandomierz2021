d77303cecf808b2befaa25e4536b8b5e994348ac
Kamil Wozniak <kamil.m.wozniak+git@gmail.com>
2021-01-14 10:28:38 +0000

    2021-01-14

--- last_intentions.php
+++ last_intentions.php
@@ -1,107 +1,109 @@
 <div class="card">
     <div class="card-header">
         <h3>INTENCJE MSZALNE<br />
-            04 – 10&nbsp;stycznia 2021&nbsp;r.
+            11 – 17&nbsp;stycznia 2021&nbsp;r.
         </h3>
     </div>
     <div class="col">
-<p class="intentions_date">04&nbsp;stycznia, Poniedziałek</p>
+<p class="intentions_date">11&nbsp;stycznia, Poniedziałek</p>
 <ul>
     <li>6.30 o&nbsp;Boże błogosławieństwo, opiekę Matki Bożej,
     potrzebne łaski i&nbsp;szczęśliwy przebieg operacji dla syna
-    Danka (1)</li>
-    <li>6.30 o&nbsp;Boże błogosławieństwo, dary Ducha Świętego,
-    potrzebne łaski i&nbsp;opiekę Matki Bożej dla dzieci,
-    i&nbsp;chrześniaków</li>
-    <li>6.30 + Klarę Stefańską</li>
-    <li>18.00 + Juliana Bednarskiego – greg.&nbsp;26</li>
-    <li>18.00 + Barbarę, Franciszka w&nbsp;4&nbsp;r.&nbsp;śm.
-    Misztalów</li>
-    <li>18.00 + Bogusławę Maziarz</li>
+    Danka (8)</li>
+    <li>6.30 + Teodozję w&nbsp;10&nbsp;r.&nbsp;śm., Zdzisława Dudków
+    i&nbsp;zm.&nbsp;z&nbsp;rodz.</li>
+    <li>6.30 + Henrykę Kurzępę – greg.&nbsp;5</li>
+    <li>18.00 o&nbsp;Boże błogosławieństwo, dary Ducha Świętego,
+    potrzebne łaski i&nbsp;opiekę Matki Bożej dla Joanny w&nbsp;dniu
+    urodzin</li>
+    <li>18.00 + Alicję, Stanisława Rodzików</li>
+    <li>18.00 + Jarosława Drabika</li>
 </ul>
 
-<p class="intentions_date">05&nbsp;stycznia, Wtorek</p>
+<p class="intentions_date">12&nbsp;stycznia, Wtorek</p>
 <ul>
     <li>6.30 o&nbsp;Boże błogosławieństwo, opiekę Matki Bożej,
     potrzebne łaski i&nbsp;szczęśliwy przebieg operacji dla syna
-    Danka (2)</li>
-    <li>6.30 o&nbsp;łaskę dobrej śmierci</li>
-    <li>18.00 dziękczynno–błagalna o&nbsp;Boże błogosławieństwo, dary
-    Ducha Świętego i&nbsp;opiekę Matki Bożej wstawiennictwo
-    św.&nbsp;Patrona dla Wojciecha Fularskiego</li>
-    <li>18.00 + Juliana Bednarskiego – greg.&nbsp;27</li>
-    <li>18.00 + Krystynę Stan</li>
+    Danka (9)</li>
+    <li>6.30 + Henrykę Kurzępę – greg.&nbsp;6</li>
+    <li>18.00 w int. parafian – domy prywatne wraz z&nbsp;szeregówką
+    przy ul.&nbsp;Maciejowskiego, ul.&nbsp;Kwiatkowskiego, Orzechowa,
+    Klonowa, Skłodowskiej, Młodożeńca, Dąbrowskiego, Wąska, Różana,
+    Szczęśliwa, Harcerska, Batalionów Chłopskich</li>
+    <li>18.00 o&nbsp;Boże błogosławieństwo, łaskę przemiany dla osób
+    uzależnionych i&nbsp;ich rodzin</li>
+    <li>18.00 o&nbsp;Boże błogosławieństwo, dary Ducha Świętego,
+    potrzebne łaski i&nbsp;opiekę Matki Bożej dla Aliny,
+    i&nbsp;Stefana Studnickich w&nbsp;rocznicę ślubu</li>
 </ul>
 
-<p class="intentions_date">06&nbsp;stycznia, Środa –
+<p class="intentions_date">13&nbsp;stycznia, Środa –
 Uroczystość Objawienia Pańskiego</p>
 <ul>
-    <li>7.30 o&nbsp;Boże błogosławieństwo, opiekę Matki Bożej,
-    potrzebne łaski i&nbsp;szczęśliwy przebieg operacji dla syna
-    Danka (3)</li>
-    <li>7.30 + Stanisława Czajkę, zm.&nbsp;z&nbsp;rodz. Czajków
-    i&nbsp;Gachów</li>
-    <li>9.30 + Zofię, Stefana Smolińskich; Jana Anielę Majewskich</li>
-    <li>11.15 Juliana Bednarskiego – greg.&nbsp;28</li>
-    <li>12.30 w&nbsp;intencji parafian</li>
-    <li>18.00 + Stanisława Kondrata; Zbigniewa Patynę; Zofię
-    Mendalę</li>
+    <li>6.30 Dziękczynno–błagalna o&nbsp;Boże błogosławieństwo, dary
+    Ducha Świętego i&nbsp;opiekę Matki Bożej dla Asi z&nbsp;racji
+    urodzin</li>
+    <li>6.30 + Henrykę Kurzępę – greg.&nbsp;7</li>
+    <li>18.00 w&nbsp;int. parafian – ul.&nbsp;Kochanowskiego,
+    Wiosenna, 15&nbsp;Sierpnia, Westerplatte, Reymonta, Asnyka,
+    Przytulna, Gałczyńskiego, Kwiatowa, Sucharzowska, Wiejska,
+    Frankowskiego, Reja, Grodzisko, tzw. os.&nbsp;Lekarskie,
+    Orzeszkowa, Modrzewskiego</li>
+    <li>18.00 Dziękczynno–błagalna o&nbsp;Boże błogosławieństwo, dary
+    Ducha Świętego i&nbsp;opiekę Matki Bożej dla Dominiki
+    z&nbsp;racji urodzin</li>
+    <li>18.00 + Weronikę Błaszczyk</li>
 </ul>
 
-<p class="intentions_date">07&nbsp;stycznia, Czwartek</p>
+<p class="intentions_date">14&nbsp;stycznia, Czwartek</p>
 <ul>
-    <li>6.30 o&nbsp;Boże błogosławieństwo, opiekę Matki Bożej,
-    potrzebne łaski i&nbsp;szczęśliwy przebieg operacji dla syna
-    Danka (4)</li>
-    <li>6.30 + Henrykę Kurzępę – greg.&nbsp;1</li>
-    <li>6.30 + Irenę Stopczyńską</li>
-    <li>18.00 + Juliana Bednarskiego – greg.&nbsp;29</li>
+    <li>6.30 o&nbsp;Boże błogosławieństwo, dary Ducha Świętego,
+    potrzebne łaski i&nbsp;opiekę Matki Bożej dla Mai Szwagierczak
+    z&nbsp;racji 6&nbsp;r.&nbsp;urodzin oraz + Tadeusza
+    i&nbsp;Adriana Szwagierczaków</li>
+    <li>6.30 + Henrykę Kurzępę – greg.&nbsp;8</li>
+    <li>18.00 w&nbsp;int. parafian - ul.&nbsp;Cieśli 1,3,7,9</li>
+    <li>18.00 + Mariannę Wójcik</li>
     <li>18.00 + Bogusławę Maziarz</li>
 </ul>
 
-<p class="intentions_date">08&nbsp;stycznia, Piątek</p>
+<p class="intentions_date">15&nbsp;stycznia, Piątek</p>
 <ul>
-    <li>6.30 o&nbsp;Boże błogosławieństwo, opiekę Matki Bożej,
-    potrzebne łaski i&nbsp;szczęśliwy przebieg operacji dla syna
-    Danka (5)</li>
-    <li>6.30 + Henrykę Kurzępę – greg.&nbsp;2</li>
-    <li>18.00 + Juliana Bednarskiego – greg.&nbsp;30</li>
+    <li>6.30 + Henrykę Kurzępę – greg.&nbsp;9</li>
+    <li>6.30 + Mariannę Wójcik</li>
+    <li>18.00 + Zdzisława Bernasia w&nbsp;r.&nbsp;śm.</li>
+    <li>18.00 + Stanisławę Jarecką</li>
+    <li>18.00 + Irenę Kotlińską</li>
+</ul>
+
+<p class="intentions_date">16&nbsp;stycznia, Sobota</p>
+<ul>
+    <li>6.30 + Henrykę Kurzępę – greg.&nbsp;10</li>
+    <li>6.30 + Bogusławę Maziarz</li>
+    <li>18.00 w&nbsp;int. parafian – ul.&nbsp;Czachowskiego
+    3,5,7,9,11,12,13,14,17,19</li>
     <li>18.00 + Mariannę Wójcik</li>
+    <li>18.00 + Tadeusza Mądrego</li>
 </ul>
 
-<p class="intentions_date">09&nbsp;stycznia, Sobota</p>
-<ul>
-    <li>6.30 o&nbsp;Boże błogosławieństwo, opiekę Matki Bożej,
-    potrzebne łaski i&nbsp;szczęśliwy przebieg operacji dla syna
-    Danka (6)</li>
-    <li>6.30 + Henrykę Kurzępę – greg.&nbsp;3</li>
-    <li>6.30 + Teresę Janas w&nbsp;5&nbsp;r.&nbsp;śm.</li>
-    <li>18.00 int. wynagradzająca Panu Bogu za&nbsp;grzechy aborcji
-    i&nbsp;eutanazji</li>
-    <li>18.00 + Zofię, Bolesława Gajewskich; Lucjana Gajewskiego;
-    Katarzynę, Władysława Kasaków; Helenę Resiak</li>
-</ul>
-
-<p class="intentions_date">10&nbsp;stycznia,
+<p class="intentions_date">17&nbsp;stycznia,
     <span style="color: grey">
-        Niedziela – Święto Chrztu Pańskiego
+        Niedziela – II Tydzień zwykły, Rok B
     </span>
 </p>
 <ul>
-    <li>7.30 o&nbsp;Boże błogosławieństwo, łaskę nawrócenia
-    i&nbsp;dar żywej wiary dla męża Piotra i&nbsp;ojca</li>
-    <li>7.30 + Michała Dudę</li>
-    <li>7.30 + Henrykę Kurzępę – greg.&nbsp;4</li>
-    <li>9.30 w&nbsp;intencji parafian</li>
-    <li>9.30 o&nbsp;Boże błogosławieństwo, opiekę Matki Bożej,
-    potrzebne łaski i&nbsp;szczęśliwy przebieg operacji dla syna
-    Danka (7)</li>
-    <li>11.15 + Kazimierza Kierzkowskiego w&nbsp;16&nbsp;r.&nbsp;śm.
-    i&nbsp;zm. z&nbsp;rodziny</li>
-    <li>12.30 dziękczynno-błagalna o&nbsp;Boże błogosławieństwo, dary
-    Ducha Świętego i&nbsp;opiekę Matki Bożej dla Legionu Maryi
-    w&nbsp;14&nbsp;r.&nbsp;powstania prezydium</li>
-    <li>18.00 + Lucjana Wódz</li>
+    <li>7.30 + Helenę, Józefa Bartłomiejczyków, Wiesława, Jana,
+    Kazimierę Wojna</li>
+    <li>7.30 + Henrykę Kurzępę – greg.&nbsp;11</li>
+    <li>9.30 + Kazimierza w&nbsp;18&nbsp;r.&nbsp;śm. Stefanię, Jana
+    Słodowników</li>
+    <li>11.15 + Zenona Kędziorę w&nbsp;3&nbsp;r.&nbsp;śm.</li>
+    <li>11.15 Dziękczynno–błagalna o&nbsp;Boże błogosławieństwo, dary
+    Ducha Świętego i&nbsp;opiekę Matki Bożej dla Kręgu Domowego
+    Kościoła w&nbsp;naszej Parafii</li>
+    <li>12.30 + Franciszka Muchę, Władysławę, Władysława, Kazimierza
+    Grudniów</li>
+    <li>18.00 w&nbsp;int. parafian</li>
 </ul>
     </div>
 </div>
